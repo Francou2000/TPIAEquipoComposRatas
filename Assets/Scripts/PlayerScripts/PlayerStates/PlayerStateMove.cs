24d32637184f52e215aa10df92cf4c1c07523a47
Franco Urbini <francourbini@gmail.com>
2024-09-16 15:03:29 -0300

    camera

--- Assets/Scripts/PlayerScripts/PlayerStates/PlayerStateMove.cs
+++ Assets/Scripts/PlayerScripts/PlayerStates/PlayerStateMove.cs
@@ -6,17 +6,30 @@ public class PlayerStateMove : State<StateEnum>
 {
     IMove _move;
     FSM<StateEnum> _fsm;
-    public PlayerStateMove(FSM<StateEnum> fsm, IMove move)
+    Transform _cameraTransform;
+
+    public PlayerStateMove(FSM<StateEnum> fsm, IMove move, Transform cameraTransform)
     {
         _move = move;
         _fsm = fsm;
+        _cameraTransform = cameraTransform;
     }
+
     public override void FixedExecute()
     {
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
 
-        Vector3 dir = new Vector3(h, 0, v);
+        Vector3 cameraForward = _cameraTransform.forward;
+        Vector3 cameraRight = _cameraTransform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 dir = (cameraForward * v + cameraRight * h).normalized;
 
         if (h == 0 && v == 0)
         {
@@ -24,8 +37,8 @@ public class PlayerStateMove : State<StateEnum>
         }
         else
         {
-            _move.Move(dir.normalized);
-            _move.Look(dir);
+            _move.Move(dir);  
+            _move.Look(dir);  
         }
     }
 }
